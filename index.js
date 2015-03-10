08972bdf57c910b85beabbe4ba91ffba45438f41
José Moreira <jmoreira@microdual.com>
2015-03-10 15:48:03 +0000

    fist commit

---
+++ index.js
@@ -0,0 +1,22 @@
+var Util = require( 'findhit-util' ),
+    Class = require( 'findhit-class' );
+
+// -----------------------------------------------------------------------------
+
+module.exports = Class.extend({
+
+    options: {
+
+
+        /**
+         * array with adapter's names
+         */
+        adapters: null,
+
+    },
+
+    initialize: function () {
+
+    },
+
+});
