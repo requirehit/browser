4e709d8a0bd06b0af7c6ee596306bcf75acdcc32
José Moreira <jmoreira@microdual.com>
2015-03-17 22:00:44 +0000

    builder + cleaner

--- README.md
+++ README.md
@@ -5,3 +5,22 @@ requirehit-browser
 
 This repo contains cross-browser code to: setup, download, decode and require
 **requirehit** packages.
+
+## Building dist file
+
+```bash
+
+  $ npm run dist-build
+
+```
+
+This will create `dist/output.js` which should be used by `requirehit` to create
+an initial bundle file.
+
+## Cleaning dist
+
+```bash
+
+  $ npm run dist-clean
+
+```
---
+++ build.js
@@ -0,0 +1,59 @@
+var pkg = require( './package' ),
+    Util = require( 'findhit-util' ),
+    fs = require( 'fs' ),
+    l = function ( l ) { console.log( "requirehit-browser: " + l ); };
+
+// -----------------------------------------------------------------------------
+
+var nativePath = 'lib/native/';
+
+// -----------------------------------------------------------------------------
+
+var output = [];
+
+//
+l( "Creating dist/ dir" );
+//
+try{
+  fs.mkdirSync( 'dist/', 0777 );
+}catch( err ){}
+
+
+//
+l( "Fetching native modules files" );
+//
+fs
+  .readdirSync( nativePath )
+  .filter(function ( filename ) {
+    return filename.substr( -3 ) === '.js';
+  })
+  .forEach(function ( filename ){
+    output.push(
+      'NativeModule._source["'+ filename +'"] = ' +
+      JSON.stringify(
+        fs.readFileSync( nativePath + filename ) + ''
+      ) +
+      ';'
+    );
+  });
+
+
+//
+l( "Wrapping output so we don't leak anything out of scope" );
+//
+output.unshift(
+  "(function () {\n"
+);
+output.push(
+  "})()"
+);
+
+
+//
+l( "saving into dist/browser.js" );
+//
+fs.writeFileSync( 'dist/browser.js', output.join( "\n" ) );
+
+
+// TODO
+// uglify it
