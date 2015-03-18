805c7f3af85c5a52e52ab698cff9f628c821dd51
José Moreira <jmoreira@microdual.com>
2015-03-18 17:52:02 +0000

    changes

--- .build.js
+++ .build.js
@@ -1,10 +1,22 @@
 var builder = require( './builder' ),
     Promise = require( 'bluebird' ),
+    UglifyJS = require( 'uglify-js' ),
     fs = Promise.promisifyAll( require( 'fs' ) );
 
 // -----------------------------------------------------------------------------
 
 builder.build()
-.then(function ( content ) {
-  fs.writeFileSync( './dist.js', content );
+.then(function ( code ) {
+  fs.writeFileSync( './dist.js', code );
+
+  var minified = UglifyJS.minify( code, {
+    fromString: true,
+    compress: true,
+  });
+
+  fs.writeFileSync( './dist.min.js', minified.code );
+
+  fs.writeFileSync( './dist.html', '<script src="dist.js"></script>' );
+  fs.writeFileSync( './dist.min.html', '<script src="dist.min.js"></script>' );
+
 });
